7fb2ddd2886241e2f3ad3aea904244dafe2eb088
KhushalJangid <kjangid897@gmail.com>
2025-04-29 11:48:56 +0530

    First commit

---
+++ src/test/java/tests/selenium/SeleniumTest.java
@@ -0,0 +1,19 @@
+package tests.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void openGoogleTest() {
+        System.setProperty("webdriver.chrome.driver", "/Users/khushal/Downloads/Makaia-master/drivers/chromedriver"); // 👈 Update path
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        assert title.contains("Google") : "Title does not contain 'Google'";
+//        driver.quit();
+    }
+}
